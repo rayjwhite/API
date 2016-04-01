840ca99fe9aa9c5cf4a1387013ef7dc174a68729
Raymond White <raymond.john.white@outlook.com>
2016-04-01 12:13:00 -0400

    Created another database object that creates and deletes the connections as needed.

---
+++ RestfulAPIs/common/dbase.js
@@ -0,0 +1,58 @@
+﻿/**
+* Created by rwhite on 4/01/2016.
+*/
+
+/** Constructor **/
+function dbase() {
+
+}
+
+/**
+ * Returns a boolean indicating if the action was completed successfully
+ * @param {string} collection
+ * @param {object} data
+ * @returns {boolean}
+ */
+dbase.prototype.Save = function (collection, data) {
+    // Connect to mongodb and raise error log error if it can't connect
+    var mongojs = require('mongojs');
+    var db = mongojs(process.env.MONGO_URI);
+    
+    // Events for db
+    db.on('error', function (err) {
+        console.log('database error while connecting to '.concat(process.env.MONGO_URI), err);
+    });
+    db.on('connect', function () {
+        console.log('database '.concat(process.env.MONGO_URI).concat(' connected'));
+    });
+    db.on('close', function () {
+        console.log('database '.concat(process.env.MONGO_URI).concat(' closed'));
+    });
+    
+    // Callback for MongoDB connection
+    var mongoCB = function (err) {
+        if (err)
+            console.log(err);
+    };
+    
+    // Open a connection to the database
+    db._getConnection(mongoCB);
+    
+    // Create our callback for the operation
+    var mongoCB = function (err) {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        // Close the database now that we are finished with it
+        db.close();
+    };
+    
+    //Create and/or add data to the data collection
+    db.collection(collection).save(data, mongoCB);
+    console.log("Completed saved to collection: " + collection);
+    return true;
+}
+
+/** Export the class **/
+module.exports = dbase;
